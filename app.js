55153e2ad5619615637cfba17988f12a444af208
Albin David C <albindavidc001@gmail.com>
2024-07-24 11:46:19 +0530

    unbound project continue

--- app.js
+++ app.js
@@ -1,51 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var ejs = require('ejs');
+const express = require('express');
+require('dotenv').config();
+const db = require('./config/db');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+db();
 
-var app = express();
-
-// View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const PORT = process.env.PORT || 3000;
 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-// Use routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// Catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
-
-
-
-
-
-// Error handler
-app.use(function(err, req, res, next) {
-  // Set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // Render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(PORT, () => {
+  console.log('The server is up and running')
+})
 
 module.exports = app;
