e488f4ac55987939c43be3e60e00f566b3f633c4
Albin David C <albindavidc001@gmail.com>
2024-07-24 13:27:08 +0530

    Finished creating directory and basic files for view engine, middleware, routes

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 require('dotenv').config();
 const db = require('./config/db');
+const path = require('path');
+const userRouter = require('./routes/userRouter');
 
 const app = express();
 db();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 
-app.listen(PORT, () => {
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.use('/',userRouter);
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('The server is up and running')
 })
 
