27c83dcbf5d1f7ac40b3964871ead064f53c352e
Albin David C <albindavidc001@gmail.com>
2024-09-11 22:54:45 +0530

    added redirection to the product from the wishlist

--- controllers/user/wishlistController.js
+++ controllers/user/wishlistController.js
@@ -5,8 +5,36 @@ module.exports = {
   getWishlist: async (req, res) => {
     try {
       userId = req.session.user;
-      const wishlist = await Wishlist.find().populate("products.productId products.addedOn userId");
-      res.render("user/wishlist", { wishlist, user: userId });
+
+      // Populate the product variants as well
+      const wishlist = await Wishlist.find({ userId }).populate("products.productId products.addedOn products.productId.variants");
+
+      let products = []; // Collect all product IDs
+      let allStocks = []; // Collect all stocks
+
+      // Iterating through the wishlist items to collect product IDs and stock data
+      wishlist.forEach((wish) => {
+        wish.products.forEach((product) => {
+          products.push(product.productId); // Collect the product ID
+
+          // Collect the stocks for each variant of the product
+          product.productId.variants.forEach((variant) => {
+            allStocks.push(variant.stock);
+          });
+        });
+      });
+
+      // Fetch all products based on the collected product IDs
+      const product = await Product.find({ _id: { $in: products.map((p) => p._id) } });
+
+      let variants ;
+      product.forEach(product =>{
+        variants = product.variants
+      })
+
+      console.log("this is variants",variants)
+
+      res.render("user/wishlist", { wishlist, user: userId, product,variants, stocks: allStocks });
     } catch (error) {
       res.redirect("user/pageNotFound");
       console.log("Internal server Error", error);
