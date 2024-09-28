8f0d4ab2e2b61ac27420edf2dd04f9c726df5f67
Albin David C <albindavidc001@gmail.com>
2024-09-28 11:27:05 +0530

    successfully updated customer page and its pagination

--- controllers/admin/customerController.js
+++ controllers/admin/customerController.js
@@ -2,75 +2,71 @@ const User = require("../../models/userSchema");
 
 const customerInfo = async (req, res) => {
   try {
-
     let search = "";
     if (req.query.search) {
       search = req.query.search;
     }
 
-    let page = 1;
-    if (req.query.page) {
-      page = req.query.page;
-    }
-
-    const limit = 3;
+    let perPage = 10;
+    let page = parseInt(req.query.page) || 1;
 
     //Fetch - user data
     const userData = await User.find({
       isAdmin: false,
-      $or: [
-        {name: {$regex: ".*" + search + ".*" }},
-        {email: {$regex: ".*" + search + ".*" }},
-      ],
+      $or: [{ name: { $regex: ".*" + search + ".*" } }, { email: { $regex: ".*" + search + ".*" } }],
     })
-    .limit(limit)
-    .skip((page-1)*limit)
-    .exec();
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .sort({ createdAt: -1 })
+      .exec();
 
-    // Count - Documents
     const count = await User.find({
-        isAdmin: false,
-        $or: [
-            {name: {$regex: ".*" + search + ".*"}},
-            {email: {$regex: ".*" + search + ".*"}},
-        ],
+      isAdmin: false,
+      $or: [{ name: { $regex: ".*" + search + ".*" } }, { email: { $regex: ".*" + search + ".*" } }],
     }).countDocuments();
 
-    // Calculate total pages
-    const totalPages = Math.ceil(count / limit);
 
-    // Render the EJS template with data
-    res.render("admin/customers", { data: userData, totalPages, currentPage: page });
-
+    const nextPage = parseInt(page) + 1;
+    const totalPages = Math.ceil(count / perPage)
+    const hasPrevPage = page > 1;
+    const hasNextPage = page < totalPages;
+    
+    res.render("admin/customers", {
+      data: userData,
+      orders: userData,
+      currentPage: page,
+      perPage: perPage,
+      nextPage,
+      hasPrevPage,
+      hasNextPage,
+      totalPages,
+    });
   } catch (error) {
-    console.error('Error fetching customer data:', error);
+    console.error("Error fetching customer data:", error);
     res.redirect("/pageerror");
   }
 };
 
-
-const customerBlocked = async (req,res) =>{
+const customerBlocked = async (req, res) => {
   try {
     let id = req.query.id;
-    await User.updateOne({_id: id}, {$set: { isBlocked:true}});
+    await User.updateOne({ _id: id }, { $set: { isBlocked: true } });
     res.redirect("/admin/users");
   } catch (error) {
-    console.error('Error blocking customer:', error);
+    console.error("Error blocking customer:", error);
     res.redirect("/pageerror");
   }
-}
+};
 
-const customerunBlocked = async (req,res) => {
+const customerunBlocked = async (req, res) => {
   try {
     let id = req.query.id;
-    await User.updateOne({_id:id}, {$set: {isBlocked:false}});
+    await User.updateOne({ _id: id }, { $set: { isBlocked: false } });
     res.redirect("/admin/users");
   } catch (error) {
     res.redirect("/pageerror");
   }
-}
-
-
+};
 
 module.exports = {
   customerInfo,
